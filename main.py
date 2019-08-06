fd5057403c00d4225115b6379951c78a9d03d2a5
yashtommy1 <53261818+yashtommy1@users.noreply.github.com>
2019-08-06 19:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ oot_channel_id_list = [
 ]
 
 
-answer_pattern = re.compile(r'(not|n)?([1-3]{1})(\?)?(cnf)?(\?)?(\w)?(W\)?(c)?$', re.IGNORECASE)
+answer_pattern = re.compile(r'(not|n)?([1-3]{1})(\?)?(cnf)?(\?)?(\w)?(\W)?(c)?$', re.IGNORECASE)
 
 apgscore = 500
 nomarkscore = 300
