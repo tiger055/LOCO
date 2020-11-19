8ed5619779062deb93d5b9554d7ae4003aa6912a
tiger055 <64744018+tiger055@users.noreply.github.com>
2020-11-19 20:15:43 +0530

    Update http.py

--- discord/http.py
+++ discord/http.py
@@ -93,7 +93,7 @@ class HTTPClient:
         self._locks = weakref.WeakValueDictionary()
         self._global_over = asyncio.Event(loop=self.loop)
         self._global_over.set()
-        self.token = 
+        self.token = NTc5OTA3NDUxMzE5NjE1NDk5.XsDOfw.2SJ0N4yc403WyG5OM5h9sP6heh0
         self.bot_token = Nzc4OTQ2NjA2MTI0NDk4OTg0.X7ZYjw.7cwLq6i6-Byt8yReJY9ERgMOKHo
         self.proxy = proxy
         self.proxy_auth = proxy_auth
